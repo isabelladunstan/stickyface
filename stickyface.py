0b3300a737ac10355db501b8a393df267792339d
Isabella Dunstan <isabelladunstan@hotmail.com>
2023-11-04 16:25:24 +1100

    Randomised choices

--- stickyface.py
+++
@@ -1,4 +0,0 @@
-import pixelate
-pixelate.pixelate('IMG_1503.jpg', 'mountain.jpg', 20)
-pixelate.pixelate('_R000222.jpg', 'flowers.jpg', 66)
-
--- test.py
+++ test.py
@@ -1,19 +1,30 @@
 import pixelate
 from matplotlib import pyplot as plt
 from matplotlib import image
+import random 
 
-labels = ['Mountain', 'Flower']
-labels[0]
+labels = ['Mountain', 'Flower', 'Ramen', 'Cake', 'Dog']
+img_folder = 'Images/'
+img_names = ['mountain.jpg', 'flower.jpg', 'ramen.jpg', 'cake.jpg', 'dog.jpg']
+
+plt.ion()  # Turn on interactive mode
+
+
+choice=random.randint(0,4)
+img_name=img_folder+img_names[choice]
+
 for pixels in range(501,0,-100):
-    pixelate.pixelate('_R000222.jpg', 'pixel.jpg', pixels)
+    pixelate.pixelate(img_name, 'pixel.jpg', pixels)
 
     img = image.imread("pixel.jpg")
     plt.imshow(img)
-    plt.show(block=False)
-    plt.pause(0.001)
+    plt.draw()  # Update the plot
+    plt.show()
 
-    sentence = "What is this image?\n1. Mountain\n2. Flowers\n3. Pretty Face\n\nChoose option or press enter to show more clearly: "
+    sentence = "What is this image?\n1. Mountain\n2. Flowers\n3. Ramen\n4. Cake\n5. Dog\n\nChoose option or press enter to show more clearly: "
 
     a = input(sentence)
 
     plt.clf()
+
+plt.ioff()  # Turn off interactive mode after loop ends
