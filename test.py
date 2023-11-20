9ada85f2c4106007e69548ca2cc745fdce0c6053
Isabella Dunstan <isabelladunstan@hotmail.com>
2023-11-20 21:48:29 +1100

    Cleaning up code and adding user choice actions

--- test.py
+++ test.py
@@ -9,24 +9,78 @@ labels = ['Mountain', 'Flower', 'Ramen', 'Cake', 'Dog']
 img_folder = 'Images/'
 img_names = ['mountain.jpg', 'flower.jpg', 'ramen.jpg', 'cake.jpg', 'dog.jpg']
 
-plt.ion()  # Turn on interactive mode
+plt.ion()  # Turn on interactive mode - means when I show the image it will keep updating
 
 # Randomising images as they appear in program
 choice=random.randint(0,4)
 randomimage=img_folder+img_names[choice]
 
 # Loop pixelating images
+# The zero is the limit of the loop i.e. can't go lower
 for pixels in range(501,0,-100):
+   
     pixelate.pixelate(randomimage, 'pixel.jpg', pixels)
 
+    # Reading the pixelated image
     img = image.imread("pixel.jpg")
+    # Display image
     plt.imshow(img)
     plt.draw()  # Update the plot
     plt.show()
 
-    sentence = "What is this image?\n1. Mountain\n2. Flowers\n3. Ramen\n4. Cake\n5. Dog\n\nChoose option or press enter to show more clearly: "
-
+    sentence = "What is this image?\n1. Mountain\n2. Flowers\n3. Ramen\n4. Cake\n5. Dog\n\nChoose option or press enter to show the image more clearly: "
+    # Function 'input' = wanting the user to put something into the terminal, and tell them what they can input using the question (sentence)
     a = input(sentence)
+
+    # User choices - conditional statements 
+    if choice==0 and a=='1':
+        print('Correct!')
+        fullimage=image.imread(randomimage)
+        # Display image
+        plt.imshow(fullimage)
+        plt.draw()  
+        plt.show()
+        # stop the loop once guess is correct
+        break 
+    elif choice==1 and a=='2':
+        print('Correct!')
+        fullimage=image.imread(randomimage)
+        # Display image
+        plt.imshow(fullimage)
+        plt.draw()  
+        plt.show()
+        # stop the loop once guess is correct
+        break 
+    elif choice==2 and a=='3':
+        print('Correct!')
+        fullimage=image.imread(randomimage)
+        # Display image
+        plt.imshow(fullimage)
+        plt.draw()  
+        plt.show()
+        # stop the loop once guess is correct
+        break 
+    elif choice==3 and a=='4':
+        print('Correct!')
+        fullimage=image.imread(randomimage)
+        # Display image
+        plt.imshow(fullimage)
+        plt.draw()  
+        plt.show()
+        # stop the loop once guess is correct
+        break 
+    elif choice==4 and a=='5':
+        print('Correct!')
+        fullimage=image.imread(randomimage)
+        # Display image
+        plt.imshow(fullimage)
+        plt.draw()  
+        plt.show()
+        # stop the loop once guess is correct
+        break 
+    # Wrong answer
+    else:
+        print('Wrong!')
 
     plt.clf()
 
