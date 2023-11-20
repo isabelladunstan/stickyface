eb09471d361cc8bc9abcefd63666965e831c6bad
Isabella Dunstan <isabelladunstan@hotmail.com>
2023-11-20 22:45:07 +1100

    Included correct and exit messages and options

--- test.py
+++ test.py
@@ -14,12 +14,15 @@ plt.ion()  # Turn on interactive mode - means when I show the image it will keep
 # Randomising images as they appear in program
 choice=random.randint(0,4)
 randomimage=img_folder+img_names[choice]
-
+# Correct image congratulations 
+winnerimg=img_folder+'Winner.png'
 # Loop pixelating images
 # The zero is the limit of the loop i.e. can't go lower
 for pixels in range(501,0,-100):
    
     pixelate.pixelate(randomimage, 'pixel.jpg', pixels)
+
+    sentence = "What is this image?\n1. Mountain\n2. Flowers\n3. Ramen\n4. Cake\n5. Dog\n\nChoose an option or press enter to show the image more clearly. To exit press 0: "
 
     # Reading the pixelated image
     img = image.imread("pixel.jpg")
@@ -28,59 +31,102 @@ for pixels in range(501,0,-100):
     plt.draw()  # Update the plot
     plt.show()
 
-    sentence = "What is this image?\n1. Mountain\n2. Flowers\n3. Ramen\n4. Cake\n5. Dog\n\nChoose option or press enter to show the image more clearly: "
     # Function 'input' = wanting the user to put something into the terminal, and tell them what they can input using the question (sentence)
     a = input(sentence)
 
     # User choices - conditional statements 
     if choice==0 and a=='1':
-        print('Correct!')
+        print('\nCorrect!')
         fullimage=image.imread(randomimage)
         # Display image
         plt.imshow(fullimage)
         plt.draw()  
         plt.show()
         # stop the loop once guess is correct
+        a=input('Press Enter!')
+        fullimage=image.imread(winnerimg) 
+        # Display image
+        plt.imshow(fullimage)
+        plt.draw()  
+        plt.show()
+        # stop the loop once guess is correct
+        a=input()
         break 
     elif choice==1 and a=='2':
-        print('Correct!')
-        fullimage=image.imread(randomimage)
+        print('\nCorrect!')
+        fullimage=image.imread(randomimage) 
         # Display image
         plt.imshow(fullimage)
         plt.draw()  
         plt.show()
         # stop the loop once guess is correct
+        a=input('Press Enter!')
+        fullimage=image.imread(winnerimg) 
+        # Display image
+        plt.imshow(fullimage)
+        plt.draw()  
+        plt.show()
+        # stop the loop once guess is correct
+        a=input()
         break 
     elif choice==2 and a=='3':
-        print('Correct!')
+        print('\nCorrect!')
         fullimage=image.imread(randomimage)
         # Display image
         plt.imshow(fullimage)
         plt.draw()  
         plt.show()
         # stop the loop once guess is correct
+        a=input('Press Enter!')
+        fullimage=image.imread(winnerimg) 
+        # Display image
+        plt.imshow(fullimage)
+        plt.draw()  
+        plt.show()
+        # stop the loop once guess is correct
+        a=input()
         break 
     elif choice==3 and a=='4':
-        print('Correct!')
+        print('\nCorrect!')
         fullimage=image.imread(randomimage)
         # Display image
         plt.imshow(fullimage)
         plt.draw()  
         plt.show()
         # stop the loop once guess is correct
+        a=input('Press Enter!')
+        fullimage=image.imread(winnerimg) 
+        # Display image
+        plt.imshow(fullimage)
+        plt.draw()  
+        plt.show()
+        # stop the loop once guess is correct
+        a=input()
         break 
     elif choice==4 and a=='5':
-        print('Correct!')
+        print('\nCorrect!')
         fullimage=image.imread(randomimage)
         # Display image
         plt.imshow(fullimage)
         plt.draw()  
         plt.show()
         # stop the loop once guess is correct
+        a=input('Press Enter!')
+        fullimage=image.imread(winnerimg) 
+        # Display image
+        plt.imshow(fullimage)
+        plt.draw()  
+        plt.show()
+        # stop the loop once guess is correct
+        a=input()
         break 
+    elif a=='0':
+        print('\nGoodbye world!')
+        plt.close()
+        break
     # Wrong answer
     else:
-        print('Wrong!')
+        print('\nWrong!')
 
     plt.clf()
 
